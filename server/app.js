ca5e030bfb4727d4159a37355364be13f4a07b5f
Ajay Singh Aswal <ajaysingh@olacabs.com>
2015-10-13 14:07:31 +0530

    :lipstick: Information

--- server/app.js
+++ server/app.js
@@ -9,11 +9,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
+var proxy = require('express-http-proxy');
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
+
+// Change the URL to the host name
+app.use('/proxy', proxy('http://www.example.com', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));
 
 // Start server
 server.listen(config.port, config.ip, function () {
